151a6e35ef79958efeaff4be30f5188f0ff9cfc2
dave-rushabh <rushabh.dave24@gmail.com>
2023-04-23 14:48:01 +0530

    Tailwind css configured

---
+++ client/constants/credits.js
@@ -0,0 +1,2 @@
+export const LOGIN_SCREEN_IMAGE_CREDITS_LINK =
+  'https://www.freepik.com/free-vector/delivery-staff-ride-motorcycles-shopping-concept_11878958.htm#query=food%20delivery%20icon&position=9&from_view=keyword&track=ais';
