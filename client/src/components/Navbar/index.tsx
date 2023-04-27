d1a2e3e3f9a34e17c8eb7bad6f517de4f5fd7aa8
dave-rushabh <rushabh.dave24@gmail.com>
2023-04-28 00:51:09 +0530

    Navbar UI initialized with logo

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,7 +1,14 @@
+import './index.css';
+
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className="navbar-wrapper">
+        <div className="nav-logo">
+          <img src="../../../assets/logo.svg" alt="" />
+          <div className="logo-title">Spice Station</div>
+        </div>
+      </div>
     </>
   );
 };
