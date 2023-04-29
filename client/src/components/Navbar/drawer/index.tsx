dc41e4a64468e851eb8b65bfcb26bdaebcc1b378
dave-rushabh <rushabh.dave24@gmail.com>
2023-04-29 14:24:21 +0530

    UI changes on the dreawer

--- client/src/components/Navbar/drawer/index.tsx
+++ client/src/components/Navbar/drawer/index.tsx
@@ -1,11 +1,9 @@
 import {
   Drawer,
   DrawerBody,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
-  Button,
 } from '@chakra-ui/react';
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 
@@ -28,25 +26,14 @@ const NavbarDrawer = ({ isOpen, onClose, btnRef }: NavbarDrawerProps) => {
         <DrawerOverlay />
         <DrawerContent>
           <div className="flex items-center h-8 px-4 justify-start my-4">
-            <div className="w-12 px-2 py-1 rounded-md bg-slate-200 hover:cursor-pointer">
-              <MdOutlineKeyboardBackspace
-                onClick={onClose}
-                size={'1.8rem'}
-                color={'#1d3557'}
-              />
+            <div
+              className="w-12 px-2 py-1 rounded-md bg-slate-200 hover:cursor-pointer"
+              onClick={onClose}
+            >
+              <MdOutlineKeyboardBackspace size={'1.8rem'} color={'#1d3557'} />
             </div>
           </div>
-          <DrawerHeader className="text-app_primary_blue">
-            Explore the world of Tesla
-          </DrawerHeader>
-
           <DrawerBody></DrawerBody>
-
-          <DrawerFooter>
-            <Button variant="outline" onClick={onClose} as="button">
-              Go Back
-            </Button>
-          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
