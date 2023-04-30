7f6914922fd370de38c7280c14beed16bb080e96
dave-rushabh <rushabh.dave24@gmail.com>
2023-04-30 17:54:36 +0530

    Protected Route and Auth Route logic maodified, instead of token availability based routing; added routing based on token's validity

--- client/src/components/AuthRoute/index.tsx
+++ client/src/components/AuthRoute/index.tsx
@@ -1,8 +1,46 @@
 import { Navigate } from 'react-router-dom';
-import { isTokenAvailable } from '../../../utils/authUtils';
+import { isTokenValid } from '../../../utils/authUtils';
+import { useEffect, useReducer } from 'react';
+
+interface ProtectedRouteState {
+  isAuthenticated: boolean | null | undefined;
+  isAnythingToBeRendered: boolean;
+}
 
 const AuthRoute = ({ children }: any) => {
-  return !isTokenAvailable() ? children : <Navigate to="/" replace />;
+  const handleTokenValidation = (state: ProtectedRouteState, action: any) => {
+    switch (action?.type) {
+      case 'SET_IS_AUTHENTICATED':
+        return {
+          ...state,
+          isAuthenticated: action?.payload,
+          isAnythingToBeRendered: true,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(handleTokenValidation, {
+    isAuthenticated: null,
+    isAnythingToBeRendered: false,
+  });
+
+  useEffect(() => {
+    const handleTokenValidation = async () => {
+      const isValid = await isTokenValid();
+      dispatch({ type: 'SET_IS_AUTHENTICATED', payload: isValid });
+    };
+    handleTokenValidation();
+  }, []);
+
+  return !state.isAnythingToBeRendered ? (
+    <></>
+  ) : state.isAuthenticated ? (
+    <Navigate to="/" replace />
+  ) : (
+    children
+  );
 };
 
 export default AuthRoute;
--- client/src/components/ProtectedRoute/index.tsx
+++ client/src/components/ProtectedRoute/index.tsx
@@ -1,12 +1,44 @@
 import { Navigate } from 'react-router-dom';
-import { isTokenAvailable } from '../../../utils/authUtils';
+import { isTokenValid } from '../../../utils/authUtils';
 import Layout from '../Layout';
+import { useEffect, useReducer } from 'react';
+
+interface ProtectedRouteState {
+  isAuthenticated: boolean | null | undefined;
+  isAnythingToBeRendered: boolean;
+}
 
 const ProtectedRoute = ({ children }: any) => {
-  return isTokenAvailable() ? (
-    <>
-      <Layout>{children}</Layout>
-    </>
+  const handleTokenValidation = (state: ProtectedRouteState, action: any) => {
+    switch (action?.type) {
+      case 'SET_IS_AUTHENTICATED':
+        return {
+          ...state,
+          isAuthenticated: action?.payload,
+          isAnythingToBeRendered: true,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(handleTokenValidation, {
+    isAuthenticated: null,
+    isAnythingToBeRendered: false,
+  });
+
+  useEffect(() => {
+    const handleTokenValidation = async () => {
+      const isValid = await isTokenValid();
+      dispatch({ type: 'SET_IS_AUTHENTICATED', payload: isValid });
+    };
+    handleTokenValidation();
+  }, []);
+
+  return !state.isAnythingToBeRendered ? (
+    <></>
+  ) : state.isAuthenticated ? (
+    <Layout>{children}</Layout>
   ) : (
     <Navigate to="/auth" replace />
   );
